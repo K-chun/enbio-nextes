cf24569f9b82a34f228d91453cb1a8dfce54700f
KoOtsuka <kou.papapaweb1003@gmail.com>
2025-11-06 13:37:23 +0900

    [include化]

---
+++ dist/include/header.php
@@ -0,0 +1,39 @@
+<header class="p-header">
+  <div class="p-header__inner">
+    <h1 class="p-header__logo">
+      <a href="/" class="p-header__logo--link">
+        <span class="p-header__logo--img">
+          <img src="/images/common/logo.svg" alt="EnBio NExTeS" width="261" height="52" loading="lazy" decoding="async">
+        </span>
+      </a>
+    </h1>
+      
+    <nav id="gnav" class="p-header__nav js-hamburger__nav" aria-label="メインメニュー" aria-hidden="true">
+      <ul class="p-header__navList">
+        <li class="p-header__navItem"><a href="/solar/" class="p-header__navLink">太陽光発電所</a></li>
+        <li class="p-header__navItem"><a href="/battery/" class="p-header__navLink">蓄電所</a></li>
+        <li class="p-header__navItem"><a href="/oandm/" class="p-header__navLink">O&amp;M</a></li>
+        <li class="p-header__navItem"><a href="/strength/" class="p-header__navLink">強み</a></li>
+        <li class="p-header__navItem"><a href="/company/" class="p-header__navLink">会社概要</a></li>
+        <li class="p-header__navItem"><a href="/news/" class="p-header__navLink">おしらせ</a></li>
+        <li class="p-header__navItem"><a href="/recruit/" class="p-header__navLink">採用情報</a></li>
+      </ul>
+  
+      <a href="/contact/" class="p-header__nav-contact u-sp">
+        <span class="p-header__nav-contact_icon"><img src="/images/common/icon_mail.svg" alt="" loading="lazy" decoding="async"></span>
+        <span class="p-header__nav-contact_text">問い合わせ</span>
+      </a>
+    </nav>
+  
+    <a href="/contact/" class="p-header__btn">
+      <span class="p-header__btn-icon"><img src="/images/common/icon_mail.svg" alt="" loading="lazy" decoding="async"></span>
+      <span class="p-header__btn-text u-pc">問い合わせ</span>
+    </a>
+  
+    <button class="p-header__hamburger js-hamburger" aria-label="ハンバーガーメニュー開閉ボタン" aria-controls="gnav" aria-expanded="false">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span> 
+    </button>
+  </div>
+</header>
